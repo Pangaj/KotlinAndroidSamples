86d98d5e86ab19468163f568910610a7180fea3c
Pangaj <pangaj@mallow-tech.com>
2017-06-13 11:14:55 +0530

    recyclerview onclickListener for kotlin as OnAddItemClickListener

--- KotlinSample/app/src/main/java/com/shruthi/pangaj/kotlinsample/activities/KSRecyclerViewActivity.kt
+++ KotlinSample/app/src/main/java/com/shruthi/pangaj/kotlinsample/activities/KSRecyclerViewActivity.kt
@@ -2,9 +2,12 @@ package com.shruthi.pangaj.kotlinsample.activities
 
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.Toast
 import com.shruthi.pangaj.kotlinsample.R
 import com.shruthi.pangaj.kotlinsample.adapters.KSRecyclerViewAdapter
+import com.shruthi.pangaj.kotlinsample.adapters.LZRecyclerItemClickListener
+
 
 /**
  * Created by Pan on 5/31/2017.
@@ -16,8 +19,7 @@ class KSRecyclerViewActivity : android.support.v7.app.AppCompatActivity() {
         setContentView(R.layout.ks_activity_recycler_view)
 
         val rvItems = findViewById(R.id.rv_items) as android.support.v7.widget.RecyclerView
-        var arrayList = ArrayList<String>
-        arrayList.add("")
+        var arrayList = ArrayList<String>()
         arrayList.add("Pangaj")
         arrayList.add("Shruthi")
 
@@ -25,5 +27,12 @@ class KSRecyclerViewActivity : android.support.v7.app.AppCompatActivity() {
         linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
         rvItems.layoutManager = linearLayoutManager
         rvItems.adapter = KSRecyclerViewAdapter(arrayList)
+
+        rvItems.addOnItemTouchListener(LZRecyclerItemClickListener(this,
+                object : LZRecyclerItemClickListener.OnItemClickListener {
+                    override fun onItemClick(view: View, position: Int) {
+                        Toast.makeText(applicationContext, "Clicked position : " + position, Toast.LENGTH_SHORT).show()
+                    }
+                }))
     }
 }
